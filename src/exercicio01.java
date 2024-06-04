17958e17240340339ee4bfea772e5171e2be23f5
erick <erickcavalhero555@gmail.com>
2024-06-03 21:56:31 -0300

    1 exercicio

---
+++ src/exercicio01.java
@@ -0,0 +1,7 @@
+//Escreva uma função que receba um array de inteiros e retorne o array ordenado em ordem crescente.
+
+public class exercicio01 {
+    public static void main(String[] args) {
+
+    }
+}
